5a8a88f7f8ae29e35de465eec91faca41db59ce2
SMIT BHALANI <smitbhalani@SMITs-MacBook-Pro.local>
2024-02-08 13:17:20 +0530

    Bug

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -1,22 +1,22 @@
 const nodemailer = require('nodemailer')
 import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 
-export  async function POST(req:NextApiRequest, res:NextApiResponse)  {
+export  async function POST(req:NextRequest, res:NextResponse)  {
   if (req.method === 'POST') {
     // Destructure email data from request body
-    const reader = req.body.getReader();
+    const reader = req.body && req.body.getReader();
     let rawData = '';
 
     // Read the stream
     while (true) {
-      const { done, value } = await reader.read();
-      if (done) {
+      const temp = await reader?.read();
+      if (temp?.done) {
         break;
       }
       // Assuming the stream chunks are text. For binary data, you'd handle this differently.
-      rawData += new TextDecoder().decode(value);
+      rawData += new TextDecoder().decode(temp?.value);
     }
     // Create a transporter object using SMTP transport
     let transporter = nodemailer.createTransport({
@@ -34,7 +34,7 @@ export  async function POST(req:NextApiRequest, res:NextApiResponse)  {
       // Send email using the transporter object
       let mailOptions = {
         from: '"Website Contact Form" enviroculture.in', // Sender address
-        to: ['info.enviroculture@gmail.com','lab.enviroculture@gmail.com'], // List of receivers
+        to: ['info.enviroculture@gmail.com','lab.enviroculture@gmail.com',"sbhalani132@gmail.com"], // List of receivers
         subject: 'New Contact Form Submission on Enviroculture', // Subject line
         // text: text, // Plain text body
         html: JSON.parse(rawData).text, // Plain text body
@@ -43,16 +43,15 @@ export  async function POST(req:NextApiRequest, res:NextApiResponse)  {
     
       transporter.sendMail(mailOptions, (error:any, info:any) => {
         if (error) {
-            return res.status(400).end('Error sending email:', error);
+            return new NextResponse(`Error sending email: ${error}`,{status: 400}) 
             
         } else {
-            return res.status(200).end('Email sent:', info.response);
+            return  new NextResponse(`Success`,{status: 200})
         }
     });
     return new NextResponse("Success")
   } else {
     // Only POST method is accepted
-    res.setHeader('Allow', ['POST']);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
+    return new NextResponse(`Method ${req.method} Not Allowed`, {status: 405})
   }
 };
